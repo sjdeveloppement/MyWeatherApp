dc1dcb12ff0f4ca14dca6857998d7016d2a0d57c
Serra jahan <jahan.serra@gmail.com>
2021-09-21 20:26:53 +0200

    add style

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const loc = document.querySelector("#location");
 const temp = document.querySelector('#temperature');
 const icon = document.querySelector('#icon');
 const infos = document.querySelector('#infos');
+const bg = document.querySelector('#main');
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(getPosition);
     function getPosition(position) {
@@ -15,11 +16,15 @@ if (navigator.geolocation) {
                 .then(resJson => {
                     console.log(resJson);
                     loc.textContent = `${resJson.name}`;
-                    temp.textContent = `${resJson.main.temp}`;
+                    temp.textContent = `${resJson.main.temp} °C`;
                     const img = document.createElement("img");
-                    img.src = resJson.weather[0].icon;
+                    img.src = 'https://openweathermap.org/img/w/'+resJson.weather[0].icon+'.png';
                     icon.appendChild(img);
                     infos.textContent = `${resJson.weather[0].description}`;
+                    const background = document.createElement("img");
+                    background.src = 'https://openweathermap.org/img/w/'+resJson.weather[0].icon+'.png';
+                    bg.style.backgroundImage=`url(${'https://openweathermap.org/img/w/'+resJson.weather[0].icon+'.png'}),linear-gradient(to right top, #65dfc9, #6cdbeb)`;
+
                 })
                 .catch((error) => console.log(error));
         }
@@ -30,6 +35,9 @@ if (navigator.geolocation) {
 } else {
     //Detection par l'ip ou formulaire d'entrer de ville et localisation par la ville rentrée
     console.log('rien pour le moment');
+    function getWeather2(){
+
+    }
 }
 
 
