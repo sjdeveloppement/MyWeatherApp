b3e2c649346778a0e298f897406dc2ef74201a80
Serra jahan <jahan.serra@gmail.com>
2021-09-22 14:10:37 +0200

    add condition if user is not geoloc use ip-api

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ if (navigator.geolocation) {
             window.fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=fr&units=metric&appid=69012ee404da614e52f21e63c243c1ed`)
                 .then(res => res.json())
                 .then(resJson => {
-                    console.log(resJson);
                     loc.textContent = `${resJson.name}`;
                     temp.textContent = `${resJson.main.temp} °C`;
                     const img = document.createElement("img");
@@ -32,12 +31,37 @@ if (navigator.geolocation) {
     }
 
 
-} else {
-    //Detection par l'ip ou formulaire d'entrer de ville et localisation par la ville rentrée
-    console.log('rien pour le moment');
-    function getWeather2(){
-
+} 
+if( navigator.geolocation.getCurrentPosition(getPosition)==undefined){
+    function getIplocation(){
+        window.fetch(`http://ip-api.com/json/`)
+        .then(response=> response.json())
+        .then(responseJson => {
+           localStorage.setItem('city',JSON.stringify(responseJson.city))
+        })
     }
-}
+    getIplocation();
+    function getWeather2(){
+        const city = localStorage.getItem('city');
+        const cityOk = JSON.parse(city);
+        window.fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityOk},fr&lang=fr&units=metric&appid=69012ee404da614e52f21e63c243c1ed`)
+        .then(res => res.json())
+                .then(resJson => {
+                    loc.textContent = `${resJson.name}`;
+                    temp.textContent = `${resJson.main.temp} °C`;
+                    const img = document.createElement("img");
+                    img.src = 'https://openweathermap.org/img/w/'+resJson.weather[0].icon+'.png';
+                    icon.appendChild(img);
+                    infos.textContent = `${resJson.weather[0].description}`;
+                    const background = document.createElement("img");
+                    background.src = 'https://openweathermap.org/img/w/'+resJson.weather[0].icon+'.png';
+                    bg.style.backgroundImage=`url(${'https://openweathermap.org/img/w/'+resJson.weather[0].icon+'.png'}),linear-gradient(to right top, #65dfc9, #6cdbeb)`;
 
+                })
+                .catch((error) => console.log(error));
+    }
+    getWeather2();
+}else{
+    console.log('utilisateur géolocalisé');
+}
 
